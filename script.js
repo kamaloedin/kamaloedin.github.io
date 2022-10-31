f637d5af0ce854a1d7d93e2d942044b64566fccf
mkama <kamalimaduddin@gmail.com>
2022-10-31 08:59:09 +0700

    Adding details button and adding the projects data in javascript file

---
+++ script.js
@@ -0,0 +1,37 @@
+const projects = [
+  {
+    id: 1,
+    title: "Todo List App",
+    img: "img/project1.jpg",
+    desc: "A simple CRUD web app built with javascript that can add lists that can be shown, edited, and deleted.",
+    imgCredit: "https://unsplash.com/@glenncarstenspeters",
+  },
+  {
+    id: 2,
+    title: "Bookshelf App",
+    img: "img/project2.jpg",
+    desc: "A simple web app that use javascript to store books' data, group them, and make them searchable.",
+    imgCredit: "https://unsplash.com/@pickawood",
+  },
+  {
+    id: 3,
+    title: "Website Page",
+    img: "img/project3.jpg",
+    desc: "A simple and clean website page built with HTML, CSS and Javascript with an article about front-end development as its content.",
+    imgCredit: "https://unsplash.com/@le_buzz",
+  },
+  {
+    id: 4,
+    title: "Rock Paper Scissor",
+    img: "img/project4.jpg",
+    desc: "A simple web-based version of Rock Paper Scissor game built with HTML, CSS, and Javascript.",
+    imgCredit: "https://unsplash.com/@marcus_wallis",
+  },
+  {
+    id: 5,
+    title: "Portfolio Website",
+    img: "img/project6.jpg",
+    desc: "A website page built with Bootstrap 5 containing some of the projects i have done. (This page)",
+    imgCredit: "https://unsplash.com/@domenicoloia",
+  },
+];
